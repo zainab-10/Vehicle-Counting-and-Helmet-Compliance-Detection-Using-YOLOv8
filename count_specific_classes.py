f7d81ac57670dba060b1335acab3e0f5f8889bd8
Zainab Bibi <91939303+zainab-10@users.noreply.github.com>
2025-01-12 23:16:23 +0500

    Add files via upload

---
+++ count_specific_classes.py
@@ -0,0 +1,37 @@
+import cv2
+
+from ultralytics import YOLO, solutions
+
+model = YOLO("best_v8.pt")
+cap = cv2.VideoCapture("test1.mp4")
+assert cap.isOpened(), "Error reading video file"
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+region_points = [(0, 0), (w, 0), (w, h), (0, h)]
+classes_to_count = [0, 1, 2, 5]
+
+# Video writer
+video_writer = cv2.VideoWriter("object_counting_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+# Init Object Counter
+counter = solutions.ObjectCounter(
+    view_img=True,
+    reg_pts=region_points,
+    classes_names=model.names,
+    draw_tracks=True,
+    line_thickness=2,
+)
+
+while cap.isOpened():
+    success, im0 = cap.read()
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    tracks = model.track(im0, persist=True, show=False, classes=classes_to_count)
+
+    im0 = counter.start_counting(im0, tracks)
+    video_writer.write(im0)
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
